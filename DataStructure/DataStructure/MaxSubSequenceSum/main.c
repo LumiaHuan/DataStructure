91d902444f2c5bf6bcf44ba1cb82d1d3c7a4a312
lumiajohn <zhanghuan@smartisan.com>
2016-08-06 22:36:18 +0800

    update others

--- DataStructure/DataStructure/MaxSubSequenceSum/main.c
+++ DataStructure/DataStructure/MaxSubSequenceSum/main.c
@@ -14,8 +14,6 @@ int main(int argc, const char * argv[]) {
     int N = sizeof(sequence) / sizeof(int);
     printIntArray(sequence, N);
     printf("Max subsequence sub is : %d\n", maxSubsequenceSum_ON3(sequence, N));
-    printf("Max subsequence sub is : %d\n", maxSubsequenceSum_ON3(sequence, N));
-
     printf("Max subsequence sub O(N2) is : %d\n", maxSubsequenceSum_ON2(sequence, N));
     printf("Max subsequence sub O(N1) is : %d\n", maxSubsequenceSum_ON1(sequence, N));
 
